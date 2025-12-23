1a8a0e0e8ddd75881cc7052d8f75151ef35b2bfd
Víctor Alfonso Orellano Calambás <74632476+Darkv1c@users.noreply.github.com>
2025-12-23 01:38:34 -0500

    Fix shebang and update oh-my-posh installation steps

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-o#!/bin/bash
+#!/bin/bash
 
 # Fix terminal type
 export TERM=xterm-256color
@@ -12,12 +12,11 @@ mv ./squashfs-root/usr/bin/nvim /usr/bin/nvim
 
 # Set up oh-my-posh
 curl -s https://ohmyposh.dev/install.sh | bash -s
-#oh-my-posh font install Hack
-#sudo cp ./Hack*.ttf /usr/share/fonts/
+oh-my-posh font install Hack
+sudo cp ./Hack*.ttf /usr/share/fonts/
 mkdir -p ~/.poshthemes
 wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
 unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
 chmod u+rw ~/.poshthemes/*.json
 rm ~/.poshthemes/themes.zip
 echo "eval \"$(oh-my-posh init bash --config ~/.poshthemes/jblab_2021.omp.json)\"" >> ~/.bashrc
-
