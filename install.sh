4c39ec36d476e718eb642b2179fdc183acd2c359
Víctor Alfonso Orellano Calambás <74632476+Darkv1c@users.noreply.github.com>
2025-12-23 01:42:25 -0500

    Simplify oh-my-posh installation process
    Removed theme setup steps and updated bashrc configuration.

--- install.sh
+++ install.sh
@@ -14,9 +14,4 @@ mv ./squashfs-root/usr/bin/nvim /usr/bin/nvim
 curl -s https://ohmyposh.dev/install.sh | bash -s
 oh-my-posh font install Hack
 sudo cp ./Hack*.ttf /usr/share/fonts/
-mkdir -p ~/.poshthemes
-wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
-unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
-chmod u+rw ~/.poshthemes/*.json
-rm ~/.poshthemes/themes.zip
-echo "eval \"$(oh-my-posh init bash --config ~/.poshthemes/jblab_2021.omp.json)\"" >> ~/.bashrc
+echo "eval \"$(oh-my-posh init bash --config /root/.cache/oh-my-posh/themes/jblab_2021.omp.json)\"" >> ~/.bashrc
