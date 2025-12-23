83d051883415906fb55a33b7ab9bca673ecae040
Víctor Alfonso Orellano Calambás <74632476+Darkv1c@users.noreply.github.com>
2025-12-23 01:21:45 -0500

    Enhance install.sh for oh-my-posh themes setup
    Added theme installation and configuration for oh-my-posh.

--- install.sh
+++ install.sh
@@ -14,4 +14,10 @@ mv ./squashfs-root/usr/bin/nvim /usr/bin/nvim
 curl -s https://ohmyposh.dev/install.sh | bash -s
 oh-my-posh font install Hack
 sudo cp ./Hack*.ttf /usr/share/fonts/
-echo "eval \"$(oh-my-posh init bash --config /usr/share/oh-my-posh/themes/jblab_2021.omp.json)\"" >> ~/.bashrc
+mkdir -p ~/.poshthemes
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
+unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
+chmod u+rw ~/.poshthemes/*.json
+rm ~/.poshthemes/themes.zip
+eval "$(oh-my-posh init bash --config ~/.poshthemes/jblab_2021.omp.json)"
+
