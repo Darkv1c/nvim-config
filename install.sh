c11ab5f34a51483477c86d13a002dd7d4d3eca2a
Víctor Alfonso Orellano Calambás <74632476+Darkv1c@users.noreply.github.com>
2025-12-24 14:29:01 -0500

    Modify nvim installation paths and cleanup
    Updated installation paths for nvim and cleaned up temporary files.

--- install.sh
+++ install.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
+set -e
 
 # Fix terminal type
 export TERM=xterm-256color
 echo 'export TERM=xterm-256color' >> ~/.bashrc
 
-# Install nvim
+# Install nvim from AppImage
 wget https://github.com/neovim/neovim/releases/download/v0.11.1/nvim-linux-x86_64.appimage
 chmod +x nvim-linux-x86_64.appimage
 ./nvim-linux-x86_64.appimage --appimage-extract
-sudo mv squashfs-root/usr/bin/nvim /usr/bin/nvim
-sudo mkdir -p /usr/share/nvim
-sudo cp -r squashfs-root/usr/share/nvim/* /usr/share/nvim/
-rm -rf squashfs-root
-sudo mkdir -p ~/.config/nvim
-sudo cp ./nvim.lua ~/.config/nvim/init.lua
+
+sudo mv squashfs-root/usr/bin/nvim /usr/local/bin/nvim
+sudo mkdir -p /usr/local/share
+sudo cp -r squashfs-root/usr/share/nvim /usr/local/share/
+
+rm -rf squashfs-root nvim-linux-x86_64.appimage
+
+# User config (sin sudo)
+mkdir -p ~/.config/nvim
+cp ./nvim.lua ~/.config/nvim/init.lua
 
 # Set up oh-my-posh
 curl -s https://ohmyposh.dev/install.sh | bash -s
